2aebef287e8b8cd06b698a8e6dacd39d7fab0d90
Sunny Sun (Pactera Technologies Inc) <v-susu@microsoft.com>
2017-03-30 11:04:49 +0800

    first commit

---
+++ requireModule.js
@@ -0,0 +1,14 @@
+ var common = require('./lib/common'),
+cons = require('./lib/const'),
+strExt = require('./lib/extString'),
+fileHelper = require('./lib/fileHelper'),
+logger = require('./lib/logger');
+
+module.exports = {
+    common: common,
+    cons:cons,
+    strExt: strExt,
+    fileHelper: fileHelper,
+    logger:logger
+};
+
